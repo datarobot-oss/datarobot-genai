4532f8aca28570dbf43969a964bd86d8bcd21cc0
Anatolii Stehnii <anatolii.stehnii@datarobot.com>
2025-10-31 14:55:07 +0200

    Tests

--- .mkfiles/tests.mk
+++ .mkfiles/tests.mk
@@ -8,7 +8,7 @@ PYTEST_OPTS_W_COVERAGE ?= $(JUNIT_FLAGS) --cov=datarobot_genai --cov-branch --co
 
 .PHONY: test
 test: ## Run repo unit tests via pytest
-	uv run $(UV_RUN_OPTS) pytest $(PYTEST_OPTS_W_COVERAGE) tests  --ignore=tests/drmcp/integration --ignore=tests/drmcp/acceptance
+	uv run $(UV_RUN_OPTS) pytest $(PYTEST_OPTS_W_COVERAGE) tests --ignore=tests/drmcp/integration --ignore=tests/drmcp/acceptance
 
 test-module: ## Run repo unit tests via pytest for a specific module
-	uv run $(UV_RUN_OPTS) pytest $(PYTEST_OPTS_W_COVERAGE) tests/$(MODULE)
+	uv run $(UV_RUN_OPTS) pytest $(PYTEST_OPTS_W_COVERAGE) tests/$(MODULE) --ignore=tests/drmcp/integration --ignore=tests/drmcp/acceptance
