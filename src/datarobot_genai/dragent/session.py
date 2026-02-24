cdc8f745bf2fd51f11d63512737018d2acb23ba2
Anatolii Stehnii <anatolii.stehnii@datarobot.com>
2026-02-24 14:31:38 +0200

    Do not modify frontend that much

---
+++ src/datarobot_genai/dragent/session.py
@@ -0,0 +1,29 @@
+# Copyright 2026 DataRobot, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from nat.runtime.session import SessionManager
+from pydantic import BaseModel
+
+from datarobot_genai.dragent.request import DRAgentRunAgentInput
+from datarobot_genai.dragent.response import DRAgentEventResponse
+
+
+class DRAgentAGUISessionManager(SessionManager):
+    def get_workflow_input_schema(self) -> type[BaseModel]:
+        """Get workflow input schema for OpenAPI documentation."""
+        return DRAgentRunAgentInput
+
+    def get_workflow_streaming_output_schema(self) -> type[BaseModel]:
+        """Get workflow streaming output schema for OpenAPI documentation."""
+        return DRAgentEventResponse
