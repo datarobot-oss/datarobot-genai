77fd7e64e687c708156e20f7e8d3244baabefb9c
Yuliia Chubatiuk <yuliia.chubatiuk@datarobot.com>
2026-05-21 18:17:44 +0300

    fix linter

--- src/datarobot_genai/langgraph/prompt.py
+++ src/datarobot_genai/langgraph/prompt.py
@@ -1,5 +1,6 @@
 from datarobot.models.genai.prompt_template import PromptTemplateVersion
 from langchain_core.prompts import ChatPromptTemplate
 
+
 def get_prompt_template(prompt_template_version: PromptTemplateVersion) -> ChatPromptTemplate:
     return ChatPromptTemplate.from_template(prompt_template_version.to_fstring())
